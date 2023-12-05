fa2a71c4f9818a9d4e0e80b57bfb9329b444831d
JoaquinHiroki <a01639134@tec.mx>
2023-12-05 16:55:07 -0600

    Initial Commit

---
+++ BerniSwift/BerniSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerniSwiftApp.swift
+//  BerniSwift
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BerniSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerniSwift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BerniSwift
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Cookies Vallarta ")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
